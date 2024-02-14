8d332758d7cf721e25d0de8d1279f3d2f7a91797
Febry Ardiansyah <febrymuhammad80@gmail.com>
2024-02-14 16:47:31 +0000

    feat: add first file

---
+++ SimpleStorage.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.8;
+// SPDX-License-Identifier: UNLICENSED
+
+contract SimpleStorage {
+    // this gets initialized to zero
+    uint256 favoriteNumber;
+
+    // map [key => string] [value => uint256]
+    mapping(string => uint256) public nameToFavoriteNumber;
+    
+    // object
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    // list/array of object
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public  {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        people.push(People(_favoriteNumber,_name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
